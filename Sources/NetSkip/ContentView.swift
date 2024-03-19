10cc0cfb604ddfc551c229184ae807454b0854b7
marcprux <mwp1@cornell.edu>
2024-03-19 14:39:07 +0100

    Update license and dependencies

--- Sources/NetSkip/ContentView.swift
+++ Sources/NetSkip/ContentView.swift
@@ -1,5 +1,5 @@
 // This is free software: you can redistribute and/or modify it
-// under the terms of the GNU Lesser General Public License 3.0
+// under the terms of the GNU General Public License 3.0
 // as published by the Free Software Foundation https://fsf.org
 
 import SwiftUI
