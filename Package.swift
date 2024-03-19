7f191dbb8ba855a14a5c452e0d19be4de8e6e7cb
marcprux <mwp1@cornell.edu>
2024-03-19 08:27:03 +0100

    Update dependencies and bundle ID

--- Package.swift
+++ Package.swift
@@ -16,7 +16,6 @@ let package = Package(
         .package(url: "https://source.skip.tools/skip.git", from: "0.8.19"),
         .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.5.11"),
         .package(url: "https://source.skip.tools/skip-ui.git", from: "0.5.10"),
-        .package(url: "https://source.skip.tools/skip-motion.git", from: "0.2.0"),
         .package(url: "https://source.skip.tools/skip-web.git", from: "0.1.0"),
         .package(url: "https://source.skip.tools/skip-model.git", from: "0.5.4"),
         .package(url: "https://source.skip.tools/skip-sql.git", from: "0.6.1"),
@@ -28,7 +27,6 @@ let package = Package(
         .target(name: "NetSkip", dependencies: [
             "NetSkipModel",
             .product(name: "SkipUI", package: "skip-ui"),
-            .product(name: "SkipMotion", package: "skip-motion"),
             .product(name: "SkipWeb", package: "skip-web"),
         ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
         .testTarget(name: "NetSkipTests", dependencies: [
@@ -37,8 +35,9 @@ let package = Package(
         ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
 
         .target(name: "NetSkipModel", dependencies: [
+            .product(name: "SkipFoundation", package: "skip-foundation"),
             .product(name: "SkipModel", package: "skip-model"),
-            .product(name: "SkipSQLPlus", package: "skip-sql"),
+            .product(name: "SkipSQL", package: "skip-sql"),
             .product(name: "SkipScript", package: "skip-script"),
             .product(name: "SkipXML", package: "skip-xml"),
             .product(name: "SkipZip", package: "skip-zip"),
