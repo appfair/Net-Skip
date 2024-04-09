d47f3d0a4b6b6cb69d505d9519a553e24d432ad7
marcprux <mwp1@cornell.edu>
2024-04-09 11:32:25 -0400

    Project updates; localization

--- Sources/NetSkipModel/NetSkipModel.swift
+++ Sources/NetSkipModel/NetSkipModel.swift
@@ -10,6 +10,64 @@ import SkipWeb
 
 let logger: Logger = Logger(subsystem: "net.skip", category: "NetSkipModel")
 
+/// Notification posted by the model when the content rules are loaded
+extension Notification.Name {
+    public static var webContentRulesLoaded: Notification.Name {
+        return Notification.Name("webContentRulesLoaded")
+    }
+}
+
+/// A store for persisting `WebBrowser` state such as history, favorites, and preferences.
+public protocol WebBrowserStore {
+    func saveItems(type: PageInfo.PageType, items: [PageInfo]) throws -> [PageInfo.ID]
+    func loadItems(type: PageInfo.PageType, ids: Set<PageInfo.ID>) throws -> [PageInfo]
+    func removeItems(type: PageInfo.PageType, ids: Set<PageInfo.ID>) throws
+}
+
+/// Information about a web page, for storing in the history or favorites list
+public struct PageInfo : Identifiable {
+    public typealias ID = Int64
+
+    /// Whether the page is a favorite bookmark or history item
+    public enum PageType {
+        case history
+        case favorite
+        case active
+    }
+
+    /// The ID of this history item if it is persistent; 0 indicates that it is new
+    public var id: ID
+    public var url: URL?
+    public var title: String?
+    public var date: Date
+
+    public init(id: ID = Int64(0), url: URL?, title: String? = nil, date: Date = Date.now) {
+        self.id = id
+        self.url = url
+        self.title = title
+        self.date = date
+    }
+}
+
+/// The configuration for a search engine
+public struct SearchEngine : Identifiable {
+    public typealias ID = String
+
+    public let id: ID
+    public let homeURL: String
+    public let name: () -> String
+    public let queryURL: (String, String) -> String?
+    public let suggestionURL: (String, String) -> String?
+
+    public init(id: String, homeURL: String, name: @escaping () -> String, queryURL: @escaping (String, String) -> String?, suggestionURL: @escaping (String, String) -> String?) {
+        self.id = id
+        self.homeURL = homeURL
+        self.name = name
+        self.queryURL = queryURL
+        self.suggestionURL = suggestionURL
+    }
+}
+
 
 extension PageInfo.PageType {
     public var tableName: String {
@@ -42,7 +100,8 @@ public class NetSkipWebBrowserStore : WebBrowserStore {
         for item in items {
             logger.log("saveItem: \(table) \(item.id)")
 
-            let url = SQLValue.text(item.url.absoluteString)
+            // URL is not nullable, but we want to be able to store a null URL, so default to blank
+            let url = SQLValue.text(item.url?.absoluteString ?? "")
             let title = item.title.flatMap { SQLValue.text($0) } ?? SQLValue.null
             let date = SQLValue.float(item.date.timeIntervalSince1970)
 
@@ -79,8 +138,6 @@ public class NetSkipWebBrowserStore : WebBrowserStore {
         }
         defer { do { try statement.close() } catch {} }
 
-        let fallbackURL = URL(string: "https://example.org")!
-
         var items: [PageInfo] = []
         while try statement.next() {
             let id = statement.integer(at: 0)
@@ -88,7 +145,7 @@ public class NetSkipWebBrowserStore : WebBrowserStore {
             let title = statement.string(at: 2)
             let date = statement.double(at: 3)
 
-            let item = PageInfo(id: id, url: URL(string: url) ?? fallbackURL, title: title, date: Date(timeIntervalSince1970: date))
+            let item = PageInfo(id: id, url: URL(string: url), title: title, date: Date(timeIntervalSince1970: date))
             items.append(item)
         }
         return items
