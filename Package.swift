084395a4fc9fb70f6092be7a1d22983ec439d61b
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-02-20 17:29:06 -0500

    Update metadata, build files, and comments

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         .package(url: "https://source.skip.tools/skip-web.git", "0.0.0"..<"2.0.0"),
         .package(url: "https://source.skip.tools/skip-sql.git", "0.0.0"..<"2.0.0"),
         .package(url: "https://source.skip.tools/skip-script.git", "0.0.0"..<"2.0.0"),
-//        .package(url: "https://source.skip.tools/skip-xml.git", "0.0.0"..<"2.0.0"),
-//        .package(url: "https://source.skip.tools/skip-zip.git", "0.0.0"..<"2.0.0"),
+        .package(url: "https://source.skip.tools/skip-miniapp.git", "0.0.0"..<"2.0.0"),
+        .package(url: "https://source.skip.tools/skip-zip.git", "0.0.0"..<"2.0.0"),
     ],
     targets: [
         .target(name: "NetSkip", dependencies: [
@@ -27,6 +27,7 @@ let package = Package(
             .product(name: "AppFairUI", package: "appfair-app"),
             .product(name: "SkipUI", package: "skip-ui"),
             .product(name: "SkipWeb", package: "skip-web"),
+            .product(name: "SkipMiniApp", package: "skip-miniapp"),
         ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
         .testTarget(name: "NetSkipTests", dependencies: [
             "NetSkip",
